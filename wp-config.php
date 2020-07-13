bb63fdf9de15b8259feee41e16d45b9087cb852e
william staley <wstaley@townnews.com>
2020-07-13 09:33:09 -0400

    new db

--- wp-config.php
+++ wp-config.php
@@ -1,6 +1,6 @@
 <?php
 # Database Configuration
-define( 'DB_NAME', 'rede' );
+define( 'DB_NAME', 'wp_publixmarketin' );
 define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'root' );
 define( 'DB_HOST', '127.0.0.1' );
